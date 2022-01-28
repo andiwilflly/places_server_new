4a52dd168dc47870e6c81c648e2c5f01693ece6a
andiwillfly <andiwillfly@gmail.com>
2022-01-28 15:58:23 +0200

    d

--- src/places/places.schema.ts
+++ src/places/places.schema.ts
@@ -1,28 +1,50 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+
+@Schema()
+class Name {
+    @Prop() 'en_US': string;
+    @Prop() 'uk_UA': string;
+    @Prop() 'ru_RU': string;
+}
+
+@Schema()
+class Description {
+    @Prop() 'en_US': string;
+    @Prop() 'uk_UA': string;
+    @Prop() 'ru_RU': string;
+}
+
 
 @Schema()
 export class Place {
-    @Prop({ required: true })
+    @Prop({required: true})
     _id: string;
 
-    @Prop({ required: true })
-    name: string;
+    @Prop({required: true})
+    name: Name;
 
-    @Prop({ required: true })
-    description: string;
+    @Prop({required: true})
+    description: Description;
 
-
-    @Prop({ required: true })
+    @Prop({required: true})
     address: string;
 
-    @Prop({ required: true })
+    @Prop({required: true})
     images: Array<string>;
 
-    @Prop({ required: true })
+    @Prop({required: true})
     videos: Array<string>;
 
     @Prop()
-    date: string;
+    lat: number;
+    @Prop()
+    long: number;
+
+    @Prop()
+    polygon: Array<Array<number>>
+
+    @Prop()
+    date: number;
 }
 
 export const PlacesSchema = SchemaFactory.createForClass(Place);
--- src/places/places.types.ts
+++ src/places/places.types.ts
@@ -1,14 +1,29 @@
 import {Document} from 'mongoose';
 
+interface IName {
+    'en_US'?: string;
+    'uk_UA'?: string;
+    'ru_RU'?: string;
+}
+interface IDescription {
+    'en_US'?: string;
+    'uk_UA'?: string;
+    'ru_RU'?: string;
+}
+
 export interface IPlace {
-    name: string;
-    description: string;
+    name: IName;
+    description: IDescription;
     address: string;
 
     images: Array<string>;
     videos: Array<string>;
 
-    date?: string;
+    lat?: number;
+    long?: number;
+    polygon?: Array<Array<number>>
+
+    date?: number;
 }
 
 export interface IPlaceDoc extends IPlace, Document {
